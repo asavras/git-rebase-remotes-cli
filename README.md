4f605ceb80ca432df7945009c9145a38433cae10
Alex Savras <raddy.by@gmail.com>
2019-04-15 01:27:58 +0300

    Create .travis.yml. Add ignore_error argument, merge options

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rebase-remotes
+
+[![Build Status](https://travis-ci.org/asavras/rebase-remotes.svg?branch=master)](https://travis-ci.org/asavras/rebase-remotes)
--- rebase-remotes.py
+++ rebase-remotes.py
@@ -6,9 +6,9 @@ import sys
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('main_branch', type=str, help='A required main branch name')
-parser.add_argument('project_path', type=str, help='A required path to the project')
-parser.add_argument('file_with_branches', type=str, help='A required path to the file with branches')
+parser.add_argument('main_branch', help='A required main branch name')
+parser.add_argument('project_path', help='A required path to the project')
+parser.add_argument('file_with_branches', help='A required path to the file with branches')
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -30,20 +30,20 @@ class RebaseRemotes(object):
         self.file_path_with_list_of_branches = file_with_list_of_branches  # type: str
 
         self.branches = []
-        self.branches_with_conflicts = []
+        self._get_list_of_branches_from_file()
 
     def _get_list_of_branches_from_file(self):
         with open(self.file_path_with_list_of_branches) as f:
             branches = f.readlines()
         self.branches = [br.replace('origin/', '').strip() for br in branches]
 
-    def _git(self, arg, raise_if_error=True):  # type: (str, bool) -> bool
+    def _git(self, arg, raise_if_error=True, ignore_error=False):  # type: (str, bool, bool) -> bool
         cmd = r'git -C {} {}'.format(self.git_repo_path, arg)
-        logger.info(cmd)
+        logger.info(arg)
         process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
 
-        if process.returncode:
+        if process.returncode and not ignore_error:
             logger.error('stdout| {}'.format(output))
             logger.error('stderr| {}'.format(error))
             if raise_if_error:
@@ -51,26 +51,43 @@ class RebaseRemotes(object):
 
         return not process.returncode
 
-    def execute(self):  # type: () -> str
-        self._get_list_of_branches_from_file()
-
+    def rebase(self):  # type: () -> str
+        assert self.branches
         self._git('fetch -p')
 
+        conflicts = []
         for branch in self.branches:
-            logger.info('Processing branch {}'.format(branch))
+            logger.info('processing {}'.format(branch))
 
-            self._git('branch -D {}'.format(branch), raise_if_error=False)
+            self._git('branch -D {}'.format(branch), ignore_error=True)
             self._git('checkout {}'.format(branch))
 
             if self._git('pull --rebase origin {}'.format(self.main_branch), raise_if_error=False):
-                self._git('push -f origin {}'.format(branch))
+                continue
+                # self._git('push -f origin {}'.format(branch))
             else:
-                self.branches_with_conflicts.append(branch)
+                conflicts.append(branch)
                 self._git('rebase --abort')
 
-        result = os.linesep.join(self.branches_with_conflicts)
+        result = os.linesep.join(conflicts)
         if not result:
-            result = 'No branches with conflicts.'
+            result = 'No branches with rebase conflicts.'
+
+        return result
+
+    def merge(self, dst_branch):
+        assert self.branches
+        self._git('checkout {}'.format(dst_branch))
+
+        conflicts = []
+        for branch in self.branches:
+            if not self._git('merge {}'.format(branch), raise_if_error=False):
+                conflicts.append(branch)
+                self._git('merge --abort')
+
+        result = os.linesep.join(conflicts)
+        if not result:
+            result = 'No branches with merge conflicts.'
 
         return result
 
@@ -78,5 +95,6 @@ class RebaseRemotes(object):
 if __name__ == '__main__':
     args = parser.parse_args()
     rebase_remotes = RebaseRemotes(args.main_branch, args.project_path, args.file_with_branches)
-    br_with_conflicts = rebase_remotes.execute()
-    print(br_with_conflicts)
+    # rebase_conflicts = rebase_remotes.rebase()
+    merge_conflicts = rebase_remotes.merge('TECH/ASR/qa_201904/BSI')
+    print(merge_conflicts)
