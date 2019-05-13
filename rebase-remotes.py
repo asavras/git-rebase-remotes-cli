8f1d02849f7a196ade14a179f67a4a145d05e3cb
Alex Savras <raddy.by@gmail.com>
2019-05-13 16:04:36 +0300

    update documentation, add some args

--- rebase-remotes.py
+++ rebase-remotes.py
@@ -1,7 +1,22 @@
 r"""
+Rebase a list of branches onto specified branch
+
+positional mandatory arguments:
+git_repo_path           path to the git repository
+file_with_branches      path to the file with list of branches
+
+--rebase -r ONTO        run rebase process onto target branch
+or
+--merge -m TARGET       run merge process to target branch
+
+optional arguments:
+--ignore -i PATH        path to the file with list of needs to ignore
+--push -p FLAG          push target branches to origin repo
+
 Examples:
-> rebase-remotes.py c:\src\ c:\list_with_branches.txt --rebase master(onto branch)
-> rebase-remotes.py c:\src\ c:\list_with_branches.txt --merge develop(target branch)
+> rebase-remotes.py c:\src\ c:\list_with_branches.txt -r master -i ignore.txt -p
+> rebase-remotes.py c:\src\ c:\list_with_branches.txt --rebase master --push --ignore c:\src\ignore.txt
+> rebase-remotes.py c:\src\ c:\list_with_branches.txt --merge develop --ignore c:\src\ignore.txt
 """
 
 import argparse
@@ -11,17 +26,8 @@ import subprocess
 import sys
 from functools import wraps
 
-from typing import Union
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('git_repo_path', help='A required path to the project')
-parser.add_argument('file_with_branches', help='A required path to the file with branches')
-parser.add_argument('--rebase', nargs=1, type=Union[str], help='Run rebase process onto target branch')
-parser.add_argument('--merge', nargs=1, type=Union[str], help='Run merge process to target branch')
-
 # create logger
-logger = logging.getLogger('main_logger')
+logger = logging.getLogger('default')
 logger.setLevel(logging.DEBUG)
 
 # create formatter for logger
@@ -38,7 +44,7 @@ logger.addHandler(ch)
 
 def print_result(func):
     @wraps(func)
-    def wrap(obj, *args, **kwargs):  # type: (GitFlow, tuple, dict) -> None
+    def wrap(obj, *args, **kwargs):  # type: (RebaseRemotes, tuple, dict) -> None
         obj.git('fetch -p')
         conflicts = func(obj, *args, **kwargs)
         result = os.linesep.join(conflicts)
@@ -50,25 +56,29 @@ def print_result(func):
     return wrap
 
 
+def filter_list_of_branches(ignore_file_name, branches):  # type: (str, list) -> list
+    with open(ignore_file_name) as f:
+        ignore_file = f.readlines()
+
+    ignore_file = set(ignore.strip('\n').upper() for ignore in ignore_file)
+    return [br for br in branches if br.split('/')[0].upper() not in ignore_file]
+
+
 def get_list_of_branches_from_file(file_name):  # type: (str) -> list
     with open(file_name) as f:
         branches = f.readlines()
 
     branches = [br.replace('origin/', '').strip() for br in branches]
 
-    ignore_file_name = 'ignore.txt'
+    ignore_file_name = args.ignore[0]
     if os.path.isfile(ignore_file_name):
-        with open(ignore_file_name) as f:
-            ignore_file = f.readlines()
+        branches = filter_list_of_branches(ignore_file_name, branches)
 
-        ignore_file = set(ignore.strip('\n').upper() for ignore in ignore_file)
-        branches = [br for br in branches if br.split('/')[0].upper() not in ignore_file]
-
-    assert branches
+    assert branches, 'the list of branches is empty'
     return branches
 
 
-class GitFlow(object):
+class RebaseRemotes(object):
 
     def __init__(self, git_repo_path, file_with_list_of_branches):
         self.git_repo_path = 'git -C {}'.format(git_repo_path)
@@ -89,13 +99,13 @@ class GitFlow(object):
         return not process.returncode
 
     @print_result
-    def rebase(self, main_branch, push=False):
+    def rebase(self, main_branch):
         conflicts = []
         for branch in self.branches:
             self.git('checkout -B {0} origin/{0}'.format(branch))
 
             if self.git('pull --rebase origin {}'.format(main_branch), interrupt_if_err=False):
-                if push:
+                if args.push:
                     self.git('push -f origin {}'.format(branch))
             else:
                 conflicts.append(branch)
@@ -119,8 +129,19 @@ class GitFlow(object):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('git_repo_path', help='path to the git repository')
+    parser.add_argument('file_with_branches', help='path to the file with list of branches')
+    parser.add_argument('--rebase', '-r', nargs=1, help='run rebase process onto target branch')
+    parser.add_argument('--merge', '-m', nargs=1, help='run merge process to target branch')
+    parser.add_argument('--ignore', '-i', nargs=1, default=[''], help='path to the file with list of needs to ignore')
+    parser.add_argument('--push', '-p', action='count', help='push target branches to origin repo')
     args = parser.parse_args()
-    rebase_remotes = GitFlow(args.git_repo_path, args.file_with_branches)
+
+    assert os.path.isdir(args.git_repo_path)
+    assert os.path.isfile(args.file_with_branches)
+
+    rebase_remotes = RebaseRemotes(args.git_repo_path, args.file_with_branches)
 
     if args.rebase:
         rebase_remotes.rebase(args.rebase[0])
