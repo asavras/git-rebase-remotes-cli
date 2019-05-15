02514b8542134af76a4c9f6770cdaacd1549e6ba
Alex Savras <raddy.by@gmail.com>
2019-05-15 17:19:55 +0300

    add test_rebase_push

--- tests/test_rebase.py
+++ tests/test_rebase.py
@@ -14,6 +14,24 @@ def test_rebase_call_order(rebase_remotes, mocker):
     rr.git.assert_has_calls(calls, any_order=False)
 
 
+def test_rebase_push(rebase_remotes, mocker):
+    mocker.patch.object(rebase_remotes, 'git', return_value=1)
+    rr = rebase_remotes
+    target_branch = 'test'
+    rr.rebase(target_branch, True)
+
+    calls = [
+        mocker.call('fetch -p'),
+        mocker.call('checkout -B {0} origin/{0}'.format('br1')),
+        mocker.call('pull --rebase origin {}'.format(target_branch), interrupt_if_err=False),
+        mocker.call('push -f origin {}'.format('br1')),
+        mocker.call('checkout -B {0} origin/{0}'.format('br2')),
+        mocker.call('pull --rebase origin {}'.format(target_branch), interrupt_if_err=False),
+        mocker.call('push -f origin {}'.format('br2')),
+    ]
+    rr.git.assert_has_calls(calls, any_order=False)
+
+
 def test_rebase_not_result(rebase_remotes, mocker):
     mocker.patch.object(rebase_remotes, 'git', return_value=1)
     rr = rebase_remotes
